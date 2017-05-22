52d58b6f669e40f504be274aeffd980c2c8bc028
Caius Brindescu <brindesc@oregonstate.edu>
2017-05-22 14:56:55 -0700

    Added the 'exported' packages

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup
 setup(name="ght_alias",
     version="0.1",
     description="Unmasking git aliases",
+    packages=['alias'],
     dependency_links=['git+git://github.com/caiusb/unicodeManager']
 )   
