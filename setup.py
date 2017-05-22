aab2ee46efd4e8faa22446775e34e428606a1bab
Caius Brindescu <brindesc@oregonstate.edu>
2017-05-22 14:53:54 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name="ght_alias",
+    version="0.1",
+    description="Unmasking git aliases",
+    dependency_links=['git+git://github.com/caiusb/unicodeManager']
+)   
