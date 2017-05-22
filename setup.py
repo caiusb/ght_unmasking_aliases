0a3299f7f93ce22a03e5f1fb7243f96a507f3b61
Caius Brindescu <brindesc@oregonstate.edu>
2017-05-22 15:28:04 -0700

    Adding regex

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(name="ght_alias",
     version="0.1",
     description="Unmasking git aliases",
     packages=['alias'],
-    install_requires=['unicodeManager']
+    install_requires=['unicodeManager', 'regex']
 )   
